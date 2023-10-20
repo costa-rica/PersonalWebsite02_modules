d06fd1ff7162c2f235f9986325f0f6a6dd18675a
Nick Rodriguez <nrodrig1@gmail.com>
2023-10-20 15:03:54 +0200

    edited setup.py for easier to install package via master branch in requirments.txt

--- pw_modules/setup.py
+++ pw_modules/setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup (
     name="pw-modules",
-    version = "0.1",
+    version="0.1",
     author="NickRodriguez",
     author_email="nick@dashanddata.com",
-    description = "pw stands for Personal Website. This is based on the started modules kit",
-    packages=['pw_config','pw_models'],
+    description="pw stands for Personal Website. This is based on the started modules kit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/costa-rica/PersonalWebsite02_modules",
+    packages=['pw_config', 'pw_models'],
     python_requires=">=3.6",
-    )
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
